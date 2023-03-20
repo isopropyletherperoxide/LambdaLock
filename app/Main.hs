1c026111df70f206d56b22c326e5129314565001
endless.sine@gmail.com <endless.sine@gmail.com>
2023-03-21 00:42:00 +0200

    added fetching passwords

--- app/Main.hs
+++ app/Main.hs
@@ -8,27 +8,42 @@ import Options.Applicative
 import System.Directory
 import System.Process
 
-
 {- Parsing Arguments -}
 
-data Commands = List {listarg :: String} | Insert {addarg :: String, dir :: FilePath} | Init {uid :: String, path :: String} 
+data Commands
+  = List {listarg :: String}
+  | Insert {addarg :: String, dir :: FilePath}
+  | Init {uid :: String, path :: String}
+  | Get {uid :: String, path :: String}
 
 newtype Options = Options {commandarg :: Commands}
 
 listP :: Parser Commands
-listP = List <$> strArgument 
-        (help "Path to the .password-store directory" <> metavar "Store Directory" <> value "HOME_FOLDER_PLACEHOLDER")
+listP =
+  List
+    <$> strArgument
+      (help "Path to the .password-store directory" <> metavar "Store Directory" <> value "HOME_FOLDER_PLACEHOLDER")
 
 addP :: Parser Commands
-addP = Insert <$> strArgument 
-        (help "Username to make an entry for" <> metavar "Username")
-        <*> strOption (long "path" <> short 'p' <> metavar "Storage Path" <> value "HOME_FOLDER_PLACEHOLDER")
+addP =
+  Insert
+    <$> strArgument
+      (help "Username to make an entry for" <> metavar "Username")
+    <*> strOption (long "path" <> short 'p' <> metavar "Storage Path" <> value "HOME_FOLDER_PLACEHOLDER")
 
+getP :: Parser Commands
+getP =
+  Get
+    <$> strArgument
+      (help "Username get the entry of" <> metavar "Entry")
+    <*> strOption (long "path" <> short 'p' <> metavar "Storage Path" <> value "HOME_FOLDER_PLACEHOLDER")
 
 initP :: Parser Commands
-initP = Insert <$> strArgument 
-        (help "Username to init the store with" <> metavar "Username")
-        <*> strOption (long "path" <> short 'p' <> metavar "Storage Path" <> value "HOME_FOLDER_PLACEHOLDER")
+initP =
+  Insert
+    <$> strArgument
+      (help "Username to init the store with" <> metavar "Username")
+    <*> strOption (long "path" <> short 'p' <> metavar "Storage Path" <> value "HOME_FOLDER_PLACEHOLDER")
 
 commandP :: Parser Options
 commandP = Options <$> subcommandP
@@ -39,15 +54,17 @@ subcommandP =
     ( command "list" (info listP (progDesc "list the current entries"))
         <> command "insert" (info addP (progDesc "insert a new entry into the password store"))
         <> command "init" (info initP (progDesc "initialize a new store"))
+        <> command "get" (info getP (progDesc "get an entry"))
     )
 
 main :: IO ()
 main = do
   passwordMg =<< execParser opts
   where
-    opts = info
+    opts =
+      info
         (commandP <**> helper)
-        ( fullDesc <> progDesc "LambdaLock")
+        (fullDesc <> progDesc "LambdaLock")
 
 {- The Real Main Function -}
 passwordMg :: Options -> IO ()
@@ -63,29 +80,39 @@ passwordMg (Options (Insert username path)) = do
     then do
       homedir <- getHomeDirectory
       setCurrentDirectory (homedir ++ passwordStore)
-      writePass username 
+      writePass username
     else do
       setCurrentDirectory path
-      writePass username 
--- TODO: This 
+      writePass username
+-- TODO: This
 passwordMg (Options (Init key path)) = do
-        if path == "HOME_FOLDER_PLACEHOLDER"
-           then do 
-                   print key
-           else do 
-                   print path
+  if path == "HOME_FOLDER_PLACEHOLDER"
+    then do
+      print key
+    else do
+      print path
+--
+passwordMg (Options (Get key path)) = do
+  if path == "HOME_FOLDER_PLACEHOLDER"
+    then do
+      homedir <- getHomeDirectory
+      setCurrentDirectory (homedir ++ passwordStore)
+      getPass key 
+    else do
+      setCurrentDirectory path
+      getPass key
 
-
-
-writePass :: FilePath -> IO () 
+writePass :: FilePath -> IO ()
 writePass filename = do
-      content <- readFile ".gpg-id"
-      let userid = stripEscapes content
-      -- print userid
-      putStr "Enter Password: "
-      password <- getLine
-      writeFile filename password
-      callCommand $ "gpg -r " ++ userid ++ " --encrypt " ++ filename 
-      removeFile filename
+  content <- readFile ".gpg-id"
+  let userid = stripEscapes content
+  -- print userid
+  putStr "Enter Password: "
+  password <- getLine
+  writeFile filename password
+  callCommand $ "gpg -r " ++ userid ++ " --encrypt " ++ filename
+  removeFile filename
 
-
+getPass :: String -> IO ()
+getPass uid = do
+  callCommand $ "gpg --decrypt " ++ uid ++ ".gpg"
